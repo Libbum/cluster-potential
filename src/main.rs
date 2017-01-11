971ca1dde09e18034f90e303584e4f224d32b2ac
Tim DuBois <tim@neophilus.net>
2017-01-11 20:00:10 +0100

    Write per zz loop in an attempt to not get os error issues on Raijin

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,7 @@ fn main() {
     let potpath = Path::new(&potname);
     let mut potfile = match File::create(&potpath) {
         Err(why) => {
-            panic!("couldn't create {}: {}",
-                   potpath.display(),
-                   why.description())
+            panic!("couldn't create {}: {}", potpath.display(), why.description())
         }
         Ok(file) => file,
     };
@@ -57,6 +55,8 @@ fn main() {
     let gry = numy * a2 - a2;
     let grz = numz * a2 - a2;
 
+    let mut input_gin = String::from("conp opti\n");
+
     for xx in 0..numxi + 5 + 1 {
         for yy in 0..numyi + 5 + 1 {
             for zz in 0..distnumz + 5 + 1 {
@@ -64,45 +64,48 @@ fn main() {
                 let tx = -(grx + 3.0 * a) + (xx as f32) * (2.0 * grx) / (numx - 1.0);
                 let ty = -(gry + 3.0 * a) + (yy as f32) * (2.0 * gry) / (numy - 1.0);
                 let tz = -(grz + 3.0 * a) +
-                         ((zz as f32) + ((node as f32) - 1.0) * (distnumz as f32)) * (2.0 * grz) /
-                         (numz - 1.0);
+                    ((zz as f32) + ((node as f32) - 1.0) * (distnumz as f32)) * (2.0 * grz) /
+                    (numz - 1.0);
                 let current = format!("O   {:.5}   {:.5}   {:.5}", tx, ty, tz);
 
-                let input_gin = "conp opti\ncart\n".to_string() + &cluster + &current +
-                                "\nlibrary streitzmintmire\n";
+                input_gin.push_str("cart\n");
+                input_gin.push_str(&cluster);
+                input_gin.push_str(&current);
+                input_gin.push_str("\nlibrary streitzmintmire\n\n");
+            }
 
-                // Spawn gulp
-                let gulp = match Command::new("./gulp")
-                    .stdin(Stdio::piped())
-                    .stdout(Stdio::piped())
-                    .spawn() {
+            // Spawn gulp
+            let gulp = match Command::new("./gulp")
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .spawn() {
                     Err(why) => panic!("couldn't spawn gulp: {}", why.description()),
                     Ok(gulp) => gulp,
                 };
 
-                // Write a string to the `stdin` of `gulp`.
-                // `stdin` has type `Option<ChildStdin>`, but since we know this instance
-                // must have one, we can directly `unwrap` it.
-                match gulp.stdin.unwrap().write_all(input_gin.as_bytes()) {
-                    Err(why) => panic!("couldn't write to gulp stdin: {}", why.description()),
-                    Ok(_) => {}
-                }
+            // Write a string to the `stdin` of `gulp`.
+            // `stdin` has type `Option<ChildStdin>`, but since we know this instance
+            // must have one, we can directly `unwrap` it.
+            match gulp.stdin.unwrap().write_all(input_gin.as_bytes()) {
+                Err(why) => panic!("couldn't write to gulp stdin: {}", why.description()),
+                Ok(_) => {}
+            }
 
-                // Because `stdin` does not live after the above calls, it is `drop`ed,
-                // and the pipe is closed.
-                //
-                // This is very important, otherwise `gulp` wouldn't start processing the
-                // input we just sent.
+            // Because `stdin` does not live after the above calls, it is `drop`ed,
+            // and the pipe is closed.
+            //
+            // This is very important, otherwise `gulp` wouldn't start processing the
+            // input we just sent.
 
-                // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
-                let mut clust_gout = String::new();
-                match gulp.stdout.unwrap().read_to_string(&mut clust_gout) {
-                    Err(why) => panic!("couldn't read gulp stdout: {}", why.description()),
-                    Ok(_) => {}
-                }
+            // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
+            let mut clust_gout = String::new();
+            match gulp.stdout.unwrap().read_to_string(&mut clust_gout) {
+                Err(why) => panic!("couldn't read gulp stdout: {}", why.description()),
+                Ok(_) => {}
+            }
 
-                let caps = re_final.captures(&clust_gout).unwrap();
-                let potval: Option<f64> = caps.get(1).and_then(|s| s.as_str().parse().ok());
+            for cap in re_final.captures_iter(&clust_gout) {
+                let potval: Option<f64> = cap.get(1).and_then(|s| s.as_str().parse().ok());
                 match potval {
                     Some(p) => {
                         let potout = format!("{:.6}\n", p * 239.2311f64);
@@ -111,9 +114,13 @@ fn main() {
                             Ok(_) => {}
                         }
                     }
-                    None => panic!("Couldn't capture final energy from gulp output."),
+                    None => panic!("Issue capturing a final energy from gulp output."),
                 }
             }
+
+            // Resetting the string this way should keep it's capacity
+            input_gin.clear();
+            input_gin.push_str("conp opti\n");
         }
     }
 
